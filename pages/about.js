662c283510afad07eee2306d522f807fa84ba133
zkaiser546 <104674077+zkaiser546@users.noreply.github.com>
2022-05-16 13:18:55 +0800

    About .js
    text paragraph

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const about = () => {
   return (
-    <div>About</div>
+    <><div>About</div><p> Celebrace is a budget-family-friendly business that helps you to commemorate memorable occasions like birthdays, anniversaries, and other special events.Celebrace offers you fast-affordable products with a wide range of presents and services such as Customize Bouquets, Balloon arrangements, invitations, ref magnets, etc.It also helps your family, friends, and loved ones celebrate their special occasion with occasional unforgettable presents to make their day more extraordinary.</p></>
   )
 }
 
