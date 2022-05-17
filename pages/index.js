61ee6ccc8e864182548972da1de68f1809469718
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2022-05-17 09:52:38 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,9 @@ import { Product, FooterBanner, HeroBanner, HeaderImage } from '../components';
 // Add the products
 // FIx the alignments and figure out how they work from youtube
 
+// https://mobprog-ils.sanity.studio/desk/product;c409ccec-7402-4b0c-b913-680d3abb61f8
+// https://mobprog-ils.vercel.app/
+
 const Home = ({ products, bannerData }) => (
   <>
     <HeaderImage />
