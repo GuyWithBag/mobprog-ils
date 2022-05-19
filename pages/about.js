9126718f8bb0d25c6c2ba6c0de0ef73dedc29449
zkaiser546 <104674077+zkaiser546@users.noreply.github.com>
2022-05-19 14:26:56 +0800

    class name

--- pages/about.js
+++ pages/about.js
@@ -11,36 +11,36 @@ const about = () => {
 
 <div className="row">
 
-    <div classNaem="image">
+    <div className="image">
         
     </div>
 
-    <div class="content">
+    <div className="content">
         <h3>BEST COMPANION FOR SPECIAL OCCASION</h3>
-        <p class="text">The reason you should pick us to be part of your
+        <p className="text">The reason you should pick us to be part of your
          special occasion is that we offer budget-friendly
          products that would part in your surprises.Also,we
-         offer free delivery in any part of Tagum City. </p>  <span class="moreText">
+         offer free delivery in any part of Tagum City. </p>  <span className="moreText">
         <p>We also accept any specific designs that you want
         in your products.You're loved ones will appreciate
         your surprise bouquet because all of our flowers are
         fresh and carefully picked by us.Our service for
         accepting orders is 24/7.</p> 
                 <div class="icons-container">
-            <div class="icons">
-                <i class="fas fa-shipping-fast"></i>
+            <div className="icons">
+                <i className="fas fa-shipping-fast"></i>
                 <span>Free Delivery</span> 
             </div>
-            <div class="icons">
-                <i class="fas fa-dollar-sign"></i>
+            <div className="icons">
+                <i className="fas fa-dollar-sign"></i>
                 <span>Easy Payments</span> 
             </div>
-            <div class="icons">
-                <i class="fas fa-headset"></i>
+            <div className="icons">
+                <i className="fas fa-headset"></i>
                 <span>24/7 service</span> 
             </div>
         </div> </span>
-        <button class="read-more-btn">Read More</button>
+        <button className="read-more-btn">Read More</button>
     </div>
 
 </div>
