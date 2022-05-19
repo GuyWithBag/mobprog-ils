e58ad37071fdd00fadf97a90f7d67841923f38b2
zkaiser546 <104674077+zkaiser546@users.noreply.github.com>
2022-05-19 12:56:29 +0800

    vision and mission

--- pages/about.js
+++ pages/about.js
@@ -3,15 +3,28 @@ import React from 'react';
 const about = () => {
   return (
     <>
+      <div class="column">
       <h3>About</h3> 
-      <div className="about-container">
+      <div className="about-container">   
         <p>
           Celebrace is a budget-family-friendly business that helps you to commemorate memorable occasions like birthdays, anniversaries, and other special events.
           Celebrace offers you fast-affordable products with a wide range of presents and services such as Customize Bouquets, Balloon arrangements, invitations, ref magnets, etc.It also helps your family, friends, 
-          and loved ones celebrate their special occasion with occasional unforgettable presents to make their day more extraordinary.
-        
-          Testoasdasdasd
+          and loved ones celebrate their special occasion with occasional unforgettable presents to make their day more extraordinary
         </p>
+        <div class="column">
+          <h3> Vision & Mission</h3>
+          <div className="VM-container"> 
+          <p>
+          Celebrace hereby offers our dreariest customers the unique, best quality affordable products they deserve for their most unforgettable moments and special occasion by striving to be the best in our craft and what we do. 
+          <ul>
+          <li>o produce affordable products that a broader population can buy </li>
+          <li>to help people commemorate their special occasions with budget-friendly presents </li>
+          <li>to establish a well-known business for gifts and surprises</li>
+         </ul>
+          </p>
+        </div>
+        </div>
+       </div>
       </div>
     </>
   )
