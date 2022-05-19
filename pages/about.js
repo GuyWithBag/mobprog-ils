4ef91a49456c4d95d437598db9d99673223ac11e
zkaiser546 <104674077+zkaiser546@users.noreply.github.com>
2022-05-19 14:19:26 +0800

    about

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,48 @@ const about = () => {
   return (
     <>
       
+<section className="about" id="about">
+
+<h3 className="sub-heading">About Us</h3>
+<h1 className="heading">Why Choose Us?</h1>
+
+<div className="row">
+
+    <div classNaem="image">
+        <image src="" alt="">
+    </div>
+
+    <div class="content">
+        <h3>BEST COMPANION FOR SPECIAL OCCASION</h3>
+        <p class="text">The reason you should pick us to be part of your
+         special occasion is that we offer budget-friendly
+         products that would part in your surprises.Also,we
+         offer free delivery in any part of Tagum City. </p>  <span class="moreText">
+        <p>We also accept any specific designs that you want
+        in your products.You're loved ones will appreciate
+        your surprise bouquet because all of our flowers are
+        fresh and carefully picked by us.Our service for
+        accepting orders is 24/7.</p> 
+                <div class="icons-container">
+            <div class="icons">
+                <i class="fas fa-shipping-fast"></i>
+                <span>Free Delivery</span> 
+            </div>
+            <div class="icons">
+                <i class="fas fa-dollar-sign"></i>
+                <span>Easy Payments</span> 
+            </div>
+            <div class="icons">
+                <i class="fas fa-headset"></i>
+                <span>24/7 service</span> 
+            </div>
+        </div> </span>
+        <button class="read-more-btn">Read More</button>
+    </div>
+
+</div>
+
+</section>
     </>
   )
 }
