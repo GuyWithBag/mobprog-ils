9e2d8cea6a6318eb47849aae7799d8960cf27808
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2022-05-21 20:31:52 +0800

    imported image directly from assets

--- pages/customer-service.js
+++ pages/customer-service.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
-
+import products from '../public/assets/images/flowers/products.png'
 const customerService = () => {
   return (
     <div className='layout-inner-contents'>
       <div>CustomerService</div>
       <Image 
-      src="/../public/assets/images/flowers/products.png"
+      src={products}
       width='300px'
       height='300px'
       />
