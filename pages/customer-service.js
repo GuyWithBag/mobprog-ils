e14b976b7e26d1d075bb02e2f90a1502c0050cc0
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2022-05-22 15:40:46 +0800

    test

--- pages/customer-service.js
+++ pages/customer-service.js
@@ -6,7 +6,7 @@ const customerService = () => {
     <div className='layout-inner-contents'>
       <div>CustomerService</div>
       <Image 
-      src='/static/assets/images/flowers/products.png'
+      src='/static/assets/images/flowers/celebrace_logo.png'
       width='300px'
       height='300px'
       />
