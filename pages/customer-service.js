871ee25db431792a33878ed29f462ac4369df162
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2022-05-22 15:47:30 +0800

    test

--- pages/customer-service.js
+++ pages/customer-service.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
-import products from '../public/static/assets/images/flowers/products.png'
+
 const customerService = () => {
   return (
     <div className='layout-inner-contents'>
       <div>CustomerService</div>
       <Image 
-      src='/static/assets/images/flowers/celebrace_logo.png'
+      src='/static/assets/images/designs/celebrace_logo.png'
       width='300px'
       height='300px'
       />
