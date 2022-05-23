603950b36ab81f940732fb92ecdd8c3db4c8c93a
zkaiser546 <104674077+zkaiser546@users.noreply.github.com>
2022-05-23 11:43:38 +0800

    try profile

--- pages/business-profile.js
+++ pages/business-profile.js
@@ -2,7 +2,46 @@ import React from 'react'
 
 const businessProfile = () => {
   return (
-    <div>BusinessProfile</div>
+    <div className="about" id="about">
+            <h3 className="sub-heading">Business-Profile</h3>
+            <h1 className="heading">Manager</h1>
+
+            <div className="row">
+
+                <div className="image">
+                    <Image src="" alt=""
+                    width='600px'
+                    height='600px'
+                    />
+                </div>
+
+                <div className="content">
+                    <h3>ERIKA FRANCES E. RACACHO </h3>
+                    <p className="text"> </p>
+
+    <div className="about1" id="about1">
+    <h3 className="sub-heading">Business-Profile</h3>
+    <h1 className="heading">Manager</h1>
+
+    <div className="row">
+
+        <div className="image">
+            <Image src="" alt=""
+            width='600px'
+            height='600px'
+            />
+        </div>
+
+        <div className="content">
+            <h3>ERIKA FRANCES E. RACACHO </h3>
+            <p className="text"> </p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+                     
   )
 }
 
