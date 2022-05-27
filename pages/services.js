669553639875daa0cfbcbe2b91045400622dbe41
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2022-05-27 21:47:30 +0800

    Update services.js

--- pages/services.js
+++ pages/services.js
@@ -4,7 +4,7 @@ import { ServiceBlock } from '../components'
 const services = () => {
   return (
     <div>
-      <ServiceBlock image={'/static/assets/images/designs/celebrace_logo.png'} text='Hi' />
+      <ServiceBlock image='/static/assets/images/designs/celebrace_logo.png' text='Hi' />
     </div>
   )
 }
