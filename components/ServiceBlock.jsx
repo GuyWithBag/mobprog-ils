11effe934591f12364743458f790880b2c7281dc
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2022-05-27 21:58:01 +0800

    test

--- components/ServiceBlock.jsx
+++ components/ServiceBlock.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import Image from 'next/image'
 
-const ServiceBlock = ( image = '/static/assets/images/designs/celebrace_logo.png', text = 'lorem ipsum') => {
+const ServiceBlock = ( text = 'lorem ipsum') => {
   return (
     <div>
-        <Image 
-        src={image}
-        width='600px'
-        height='600px'
-        />
         <p>
             {text}
         </p>
--- pages/services.js
+++ pages/services.js
@@ -4,7 +4,7 @@ import { ServiceBlock } from '../components'
 const services = () => {
   return (
     <div>
-      <ServiceBlock image='/static/assets/images/designs/celebrace_logo.png' text='Hi' />
+      <ServiceBlock text='Hi bitch' />
     </div>
   )
 }
